a26c72bd30f4ed1c836ff125230937bcdb9103a4
JavierStark <javiertorralbocortes@gmail.com>
2024-05-22 13:37:03 +0200

    feat: splitted pane for image and map

--- src/main/java/org/umaguessr/frontend/HideablePanel.java
+++ src/main/java/org/umaguessr/frontend/HideablePanel.java
@@ -1,41 +1,22 @@
 package org.umaguessr.frontend;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class HideablePanel extends JPanel {
     public HideablePanel(){
         super();
-        setLayout(null);
         setBackground(java.awt.Color.LIGHT_GRAY);
+        setSize(new Dimension(800, 500));
+        setLayout(new BorderLayout());
+        setBorder(new EmptyBorder(10, 10, 10, 10));
 
         ZoomableImagePanel zoomableImagePanel = new ZoomableImagePanel(1.1);
-        zoomableImagePanel.setBounds(0, 0, 800, 100);
+        zoomableImagePanel.setSize(new Dimension(800, 600));
+        zoomableImagePanel.setVisible(true);
         add(zoomableImagePanel);
-        addMouseListener(new java.awt.event.MouseAdapter() {
-            @Override
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                System.out.println("Mouse entered");
-                zoomableImagePanel.setBounds(0, 0, 800, 250);
-                setSize(new Dimension(800, 300));
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                System.out.println("Mouse entered");
-
-                Rectangle r = e.getComponent().getBounds();
-                Point p = e.getPoint();
-
-                if (p.x < 0 || p.x >= r.width
-                        ||  p.y < 0 || p.y >= r.height) {
-                    zoomableImagePanel.setBounds(0, 0, 800, 150);
-                    setSize(new Dimension(800, 200));
-                }
-            }
-        });
-        setSize(new Dimension(800, 200));
         setVisible(true);
     }
 }
--- src/main/java/org/umaguessr/frontend/UI.java
+++ src/main/java/org/umaguessr/frontend/UI.java
@@ -14,14 +14,26 @@ public class UI extends JFrame {
         setBackground(java.awt.Color.WHITE);
 
 
-        add(new HideablePanel());
-        add(new ZoomableImagePanel(1.1));
+        ZoomableImagePanel zoomableImagePanel = new ZoomableImagePanel(1.1);
+        zoomableImagePanel.setSize(new Dimension(800, 600));
+        zoomableImagePanel.setVisible(true);
 
 
-        setVisible(true);
+        HideablePanel hideablePanel = new HideablePanel();
+        hideablePanel.setSize(new Dimension(800, 200));
+        hideablePanel.setVisible(true);
 
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, zoomableImagePanel, hideablePanel);
+        splitPane.setDividerSize(1);
+
+        JPanel content = new JPanel();
+        content.setLayout(new BorderLayout());
+        content.add(splitPane, BorderLayout.CENTER);
+
+        setContentPane(content);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setPreferredSize(new Dimension(500, 400));
         pack();
-        setSize(800, 600);
-
+        setVisible(true);
     }
 }
