254364c7a9a8b42c5ee53833bb976b71dfbbcc2b
Damián <admin@damianverde.es>
2024-05-23 11:11:14 +0200

    refactor: improve readability and maintainability of ScoreService

--- src/main/java/org/umaguessr/backend/ScoreService.java
+++ src/main/java/org/umaguessr/backend/ScoreService.java
@@ -3,54 +3,45 @@ package org.umaguessr.backend;
 public class ScoreService {
 
 	private int finalScore;
-	private ImageService imageService;
+	private final ImageService imageService;
 
-	
 	public static final int MAX_SCORE = 100;
-	
-	
+	private static final int MIN_DISTANCE_FOR_MAX_SCORE = 15;
+	private static final int MAX_DISTANCE_FOR_ZERO_SCORE = 250;
+	private static final double BASE = 1.06;
+	private static final double EXPONENT_MULTIPLIER = -0.3;
+	private static final double EXPONENT_CONSTANT = 87;
+
 	public ScoreService(ImageService imageService) {
 		this.imageService = imageService;
-		finalScore = 0;
+		this.finalScore = 0;
 	}
-	
-	
+
 	public int calculateScore(String id, int coordX, int coordY) {
-		
-		int points;
-		double dist = calculateDistance(id, coordX, coordY);
-		
-		if (dist <= 15) {
-			
-			points = MAX_SCORE;
-			
-		} else if (dist > 250) {
-			
-			points = 0;
-			
-		} else {
-			// 1.06^{-(0.3x - 87)}, being x the distance from the actual coordinate.
-			points = (int) Math.ceil(Math.pow(1.06, -(0.3*dist - 87)));
-					
-		}
-		
+		double distance = calculateDistance(id, coordX, coordY);
+		int points = calculatePointsBasedOnDistance(distance);
 		finalScore += points;
 		return points;
-		
 	}
-	
+
 	public int getFinalScore() {
-		return this.finalScore;
+		return finalScore;
 	}
-	
-	
+
 	private double calculateDistance(String id, int coordX, int coordY) {
 		Image image = imageService.getImageData(id);
-		
 		int differenceX = image.getXCoordinate() - coordX;
 		int differenceY = image.getYCoordinate() - coordY;
-		
-		return Math.sqrt((double)(differenceX*differenceX) - differenceY*differenceY);
+		return Math.sqrt(differenceX * differenceX + differenceY * differenceY);
 	}
-	
+
+	private int calculatePointsBasedOnDistance(double distance) {
+		if (distance <= MIN_DISTANCE_FOR_MAX_SCORE) {
+			return MAX_SCORE;
+		} else if (distance > MAX_DISTANCE_FOR_ZERO_SCORE) {
+			return 0;
+		} else {
+			return (int) Math.ceil(Math.pow(BASE, EXPONENT_MULTIPLIER * distance + EXPONENT_CONSTANT));
+		}
+	}
 }
