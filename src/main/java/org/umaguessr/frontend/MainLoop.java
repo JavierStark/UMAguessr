c008d54fac932c60ffa21427e4e6d5f78a30b25a
Torpinped <torpinped@uma.es>
2024-05-24 13:38:46 +0200

    Added new menu classes.

---
+++ src/main/java/org/umaguessr/frontend/MainLoop.java
@@ -0,0 +1,28 @@
+package org.umaguessr.frontend;
+
+import javax.swing.UIManager;
+
+public class MainLoop {
+	
+	public static void main(String args[]) {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+        	public void run() {
+                StartingMenu_panel frame = new StartingMenu_panel();
+                frame.setVisible(true);
+            }
+        });
+    }
+	
+}
