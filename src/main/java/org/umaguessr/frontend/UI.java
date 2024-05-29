e9ba1d1be2806239e5db420bebcb47d6bb974234
SebastianTGD1 <sebastiantgd@uma.es>
2024-05-29 14:16:09 +0200

    build: scorePanel changes when the game is played

--- src/main/java/org/umaguessr/frontend/UI.java
+++ src/main/java/org/umaguessr/frontend/UI.java
@@ -15,6 +15,7 @@ import java.net.URISyntaxException;
 public class UI extends JFrame {
     ImageService imageService;
     ScoreService scoreService;
+    ZoomableImagePanel zoomableImagePanel;
 
     public UI(ImageService imageService, ScoreService scoreService) throws IOException, URISyntaxException {
         super();
@@ -29,7 +30,7 @@ public class UI extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBackground(java.awt.Color.WHITE);
 
-        ZoomableImagePanel zoomableImagePanel = new ZoomableImagePanel(1.2, imageService.readImageFromURL(image.getURL()));
+        zoomableImagePanel = new ZoomableImagePanel(1.2, imageService.readImageFromURL(image.getURL()));
         zoomableImagePanel.setSize(new Dimension(800, 600));
         zoomableImagePanel.setVisible(true);
 
@@ -46,7 +47,7 @@ public class UI extends JFrame {
         splitPane.setDividerLocation(200);
 
 
-        JButton signalButton = getSignalButton(imageID);
+        JButton signalButton = getSignalButton(imageID, scorePanel);
 
         JPanel content = new JPanel();
         content.setLayout(new BorderLayout());
@@ -61,7 +62,7 @@ public class UI extends JFrame {
         setVisible(true);
     }
 
-    private JButton getSignalButton(String imageID) {
+    private JButton getSignalButton(String imageID, ScorePanel scorePanel) {
         JButton signalButton = new JButton("Send Signal");
 
         // Add an ActionListener to the button
@@ -73,10 +74,24 @@ public class UI extends JFrame {
                 System.out.println(this.scoreService.calculateScore(imageID, (int) marker.getX(), (int) marker.getY()));
                 System.out.println(previousMarker.getRealX() + " " + previousMarker.getRealY());
             }
+            scorePanel.setScore(this.scoreService.getFinalScore());
+            scorePanel.nextRound();
+            String imageID2 = this.imageService.getRandomUnplayedImageId();
+            Image image2 = this.imageService.getImageData(imageID2);
+            changeImage(image2);
         });
 
         signalButton.setVisible(true);
         signalButton.setPreferredSize(new Dimension(200, 100));
         return signalButton;
     }
+    
+	private void changeImage(Image image) {
+        try {
+			zoomableImagePanel.setImage(imageService.readImageFromURL(image.getURL()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
 }
