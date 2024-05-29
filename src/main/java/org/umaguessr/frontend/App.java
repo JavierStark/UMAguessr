deeb3098158a2539a8d6806901da43dc500566bc
JavierStark <javiertorralbocortes@gmail.com>
2024-05-29 14:11:39 +0200

    style: change lookandfeel to windows

--- src/main/java/org/umaguessr/frontend/App.java
+++ src/main/java/org/umaguessr/frontend/App.java
@@ -14,6 +14,12 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                     UnsupportedLookAndFeelException e) {
+                throw new RuntimeException(e);
+            }
             ImageService imageService = new ImageService();
             ScoreService scoreService = new ScoreService(imageService);
             try {
