46704d614ceddd208f6474099006326464fc15e8
Damián <admin@damianverde.es>
2024-05-30 16:27:59 +0200

    feat: replace JSON data source with PostgreSQL and update tests

--- src/test/java/backend/ScoreServiceTest.java
+++ src/test/java/backend/ScoreServiceTest.java
@@ -11,12 +11,15 @@ import org.umaguessr.backend.ScoreService;
 import org.umaguessr.backend.Image;
 import org.umaguessr.backend.ImageService;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 class ScoreServiceTest {
 
 	ScoreService myScoreService;
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws IOException, SQLException {
 		ImageService imageService = new ImageService();
 		myScoreService = new ScoreService(imageService);
 	}
@@ -28,31 +31,29 @@ class ScoreServiceTest {
 
 	@Test
 	void testMaxScoreOnExactLocation() {
-		assertEquals(ScoreService.MAX_SCORE, myScoreService.calculateScore("img101", 227, 721));
+		assertEquals(ScoreService.MAX_SCORE, myScoreService.calculateScore("1", 227, 721));
 	}
 
 	@Test
 	void testSendingCoordinatesFurtherThan200Score0() {
-		assertEquals(0, myScoreService.calculateScore("img105", 7, 27));
+		assertEquals(0, myScoreService.calculateScore("5", 7, 27));
 	}
 
 	@Test
 	void testFinalScoreIncreasesEachRound() {
-		myScoreService.calculateScore("img101", 227, 721);
+		myScoreService.calculateScore("1", 227, 721);
 		assertEquals(ScoreService.MAX_SCORE, myScoreService.getFinalScore());
 
-		myScoreService.calculateScore("img105", 124, 796);
+		myScoreService.calculateScore("5", 124, 796);
 		assertEquals(2*ScoreService.MAX_SCORE, myScoreService.getFinalScore());
 
-		myScoreService.calculateScore("img102", 238, 790);
+		myScoreService.calculateScore("2", 238, 790);
 		assertEquals(3*ScoreService.MAX_SCORE, myScoreService.getFinalScore());
-
 	}
 
 	// white box testing
 	@Test
 	void testLessScoreOnFurtherFromSolution() {
-		assertFalse(myScoreService.calculateScore("img101", 100, 200) > myScoreService.calculateScore("img101", 80, 100));
+		assertFalse(myScoreService.calculateScore("1", 100, 200) > myScoreService.calculateScore("1", 80, 100));
 	}
-
 }
