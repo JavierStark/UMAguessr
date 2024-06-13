eba5db81180b0bb3d9dfaeb2a9c9dc6b8372529c
SebastianTGD1 <sebastiantgd@uma.es>
2024-06-13 10:06:17 +0200

    Refactor: refactored HideablePanel and MainLoop

--- src/main/java/org/umaguessr/frontend/HideablePanel.java
+++ src/main/java/org/umaguessr/frontend/HideablePanel.java
@@ -9,14 +9,16 @@ import java.awt.image.BufferedImage;
 
 public class HideablePanel extends JPanel {
     public HideablePanel(BufferedImage image){
-        super();
+    	super();
+    	final int width = 800;
+    	final int height = 600;
         setBackground(java.awt.Color.LIGHT_GRAY);
-        setSize(new Dimension(800, 500));
+        setSize(new Dimension(width, height-100));
         setLayout(new BorderLayout());
         setBorder(new EmptyBorder(10, 10, 10, 10));
 
         ZoomableImagePanel zoomableImagePanel = new ZoomableImagePanel(1.2, image);
-        zoomableImagePanel.setSize(new Dimension(800, 600));
+        zoomableImagePanel.setSize(new Dimension(width, height));
         zoomableImagePanel.setVisible(true);
         zoomableImagePanel.addMarkers();
         add(zoomableImagePanel);
